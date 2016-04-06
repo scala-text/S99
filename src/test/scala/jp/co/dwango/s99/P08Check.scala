62dd434566159667d7541abf07acfe6b5c258e08
Kota Mizushima <mizukota@gmail.com>
2016-04-06 15:18:59 +0900

    Tests for P08

---
+++ src/test/scala/jp/co/dwango/s99/P08Check.scala
@@ -0,0 +1,21 @@
+package jp.co.dwango.s99
+
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Properties
+
+class P08Check extends Properties("P08") {
+  def duplicates(list: List[Int]): Int = {
+    def loop(list: List[Int], curr: Int, count: Int): Int = list match {
+      case Nil => count
+      case x::xs =>
+        if(curr == x) loop(xs, curr, count + 1) else loop(xs, x, count)
+    }
+    list match {
+      case x::xs => loop(xs, x, 0)
+      case Nil => 0
+    }
+  }
+  property("compress()") = forAll{ (s: List[Int]) =>
+    P08.compress(s).length == s.length - duplicates(s)
+  }
+}
