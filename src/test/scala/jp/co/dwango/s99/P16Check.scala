d3eb5c9d9cd0773475e455b8a0c1cbfa5514cb37
Kota Mizushima <mizukota@gmail.com>
2016-04-07 00:08:59 +0900

    Fix the param of chooseNum to reflect the review.

--- src/test/scala/jp/co/dwango/s99/P16Check.scala
+++ src/test/scala/jp/co/dwango/s99/P16Check.scala
@@ -5,8 +5,8 @@ import Gen.listOf, Gen.chooseNum
 import org.scalacheck.Prop.{BooleanOperators, forAll}
 
 class P16Check extends Properties("P16") {
-  property("drop()") = forAll(listOf(chooseNum(Int.MinValue, Int.MaxValue)), chooseNum(-1, 10)){ (s: List[Int], i: Int) =>
-    (0 < i && i <= s.length) ==> (P16.drop(i, s) == s.zipWithIndex.map{ case (e, j) =>(e, j +1) }.filterNot { case (e, j) =>
+  property("drop()") = forAll(listOf(chooseNum(Int.MinValue, Int.MaxValue)), chooseNum(1, 10)){ (s: List[Int], i: Int) =>
+    (P16.drop(i, s) == s.zipWithIndex.map{ case (e, j) =>(e, j +1) }.filterNot { case (e, j) =>
       j % i == 0
     }.map{_._1})
   }
