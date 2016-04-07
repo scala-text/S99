c4ac1a4d4a14217524931a9795868b1fac81be0d
Kota Mizushima <mizukota@gmail.com>
2016-04-07 12:08:12 +0900

    Add tests for P18

---
+++ src/test/scala/jp/co/dwango/s99/P18Check.scala
@@ -0,0 +1,11 @@
+package jp.co.dwango.s99
+
+import org.scalacheck.{Gen, Properties}
+import Gen.listOf, Gen.chooseNum
+import org.scalacheck.Prop.{BooleanOperators, forAll}
+
+class P18Check extends Properties("P18") {
+  property("slice()") = forAll(listOf(chooseNum(Int.MinValue, Int.MaxValue)), chooseNum(0, 10), chooseNum(0, 10)){ (s: List[Int], i: Int, j: Int) =>
+    (i <= j && j <= s.length) ==> (P18.slice(i, j, s) == s.slice(i, j))
+  }
+}
