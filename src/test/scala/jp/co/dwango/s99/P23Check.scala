8441012f1a516366f18b11a6a3c1786df6fe181e
Kota Mizushima <mizukota@gmail.com>
2016-04-07 16:47:59 +0900

    Add tests for P23

---
+++ src/test/scala/jp/co/dwango/s99/P23Check.scala
@@ -0,0 +1,16 @@
+package jp.co.dwango.s99
+
+import org.scalacheck.{Arbitrary, Gen, Prop, Properties}
+
+class P23Check extends Properties("P23") {
+  property("randomSelect()") = {
+    val gen = for {
+      n <- Gen.choose(0, 100)
+      s <- Gen.listOfN(n, implicitly[Arbitrary[Int]].arbitrary)
+    } yield (s, n)
+
+    Prop.forAll(gen){ case (s, n) =>
+      P23.randomSelect(n, s).length == n
+    }
+  }
+}
