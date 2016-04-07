ecba333f5cdc4f4e85c269130d790730e4b3c0a5
Kota Mizushima <mizukota@gmail.com>
2016-04-07 23:27:31 +0900

    Tests of P28b

---
+++ src/test/scala/jp/co/dwango/s99/P28bCheck.scala
@@ -0,0 +1,20 @@
+package jp.co.dwango.s99
+
+import org.scalacheck.{Prop, Gen, Arbitrary, Properties}
+
+class P28bCheck extends Properties("P28b") {
+  property("lsortFreq()") = {
+    Prop.forAll{ (s: List[List[Int]]) =>
+      val freqs = s.foldLeft(Map.empty[Int, Int]){case (m, e) =>
+        val value = m.getOrElse(e.length, 0)
+        m.updated(e.length, value + 1)
+      }
+      val a = P28b.lsortFreq(s)
+      if(a.length < 2) {
+        true
+      } else {
+        a.zip(a.tail).forall{case (l, r) => freqs(l.length) <= freqs(r.length)}
+      }
+    }
+  }
+}
