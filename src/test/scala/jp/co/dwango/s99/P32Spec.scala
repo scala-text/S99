7239ecc37ee56edc1eb8f8748b001f2c7bd9a840
3tty0n <yuizalp@gmail.com>
2016-04-26 03:17:37 +0900

    Add test for Int.MaxValue

--- src/test/scala/jp/co/dwango/s99/P32Spec.scala
+++ src/test/scala/jp/co/dwango/s99/P32Spec.scala
@@ -13,6 +13,7 @@ class P32Spec extends FunSpec with DiagrammedAssertions {
       assert(P32.gcd(3 * 5 * 7 * 11, 3 * 5 * 7 * 11 * 17 * 19) == 3 * 5 * 7 * 11)
       assert(P32.gcd(3, 3) == 3)
       assert(P32.gcd(2 * 3 * 5 * 7, 2 * 3 * 5 * 17)  == 2 * 3 * 5)
+      assert(P32.gcd(Int.MaxValue, Int.MaxValue) == Int.MaxValue)
     }
     it("for negative integer numbers") {
       assert(P32.gcd(-5, -5) == 5)
