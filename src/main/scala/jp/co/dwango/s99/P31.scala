ab9dc84f44207ad88e6bd94f34d0030b720bea3f
3tty0n <yuizalp@gmail.com>
2016-04-26 19:18:01 +0900

    use implicit class

--- src/main/scala/jp/co/dwango/s99/P31.scala
+++ src/main/scala/jp/co/dwango/s99/P31.scala
@@ -1,10 +1,10 @@
 package jp.co.dwango.s99
 
 object P31 {
-  implicit def isPrime(x: Int) = new {
+  implicit class RichInt(self: Int) {
     def isPrime(): Boolean = {
-      val nums = 2 to x
-      (x > 1) && nums.takeWhile(_ <= Math.sqrt(x)).forall(x % _ != 0)
+      val nums = 2 to self
+      (self > 1) && nums.takeWhile(_ <= Math.sqrt(self)).forall(self % _ != 0)
     }
   }
 }
