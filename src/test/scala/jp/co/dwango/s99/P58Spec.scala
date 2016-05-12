1e0d880d310aa99e821499800275b74bc0134bf3
Kota Mizushima <mizukota@gmail.com>
2016-05-12 15:11:19 +0900

    Answer and test of P58

---
+++ src/test/scala/jp/co/dwango/s99/P58Spec.scala
@@ -0,0 +1,19 @@
+package jp.co.dwango.s99
+
+import jp.co.dwango.s99.P58.Tree
+import jp.co.dwango.s99.binary_trees.{Node, End}
+import org.scalatest.{DiagrammedAssertions, FunSpec}
+
+class P58Spec extends FunSpec with DiagrammedAssertions {
+  describe("P58") {
+    it("""Tree.symmetricBalancedTrees(5, "x")""") {
+      assert(
+        List(
+          Node("x", Node("x", End, Node("x")), Node("x", Node("x", End, End), End)),
+          Node("x", Node("x", Node("x"), End), Node("x", End, Node("x")))
+        ) ==
+        Tree.symmetricBalancedTrees(5, "x")
+      )
+    }
+  }
+}
