073ea7e50d1286ea733cf0eb97cad521cf782818
Tomohisa Osada <agekuta@gmail.com>
2017-05-19 11:25:07 +0900

    modified answer

--- src/main/scala/jp/co/dwango/s99/P96.scala
+++ src/main/scala/jp/co/dwango/s99/P96.scala
@@ -11,56 +11,39 @@ object P96 {
   case class Failure(msg:String) extends Result
   case class Success(rest:String) extends Result
 
-  trait Parser extends (String => Result) {
-    def ~(that: => Parser) = {
-      val fn : String => Result = input => this.apply(input) match{
+  class Parser(fn : String => Result) extends (String => Result) {
+    override def apply(v1: String): Result = fn(v1)
+    def ~(that: => Parser) : Parser =
+      new Parser(input => this.apply(input) match {
         case f@Failure(_) => f
         case Success(rest) => that(rest)
-      }
-      new Parser {
-        override def apply(v1: String): Result = fn(v1)
-      }
-    }
-    def ? = {
-      val fn : String => Result = input => this.apply(input) match{
+      })
+    def ? : Parser =
+      new Parser(input => this.apply(input) match {
         case Failure(_) => Success(input)
         case s@Success(_) => s
-      }
-      new Parser {
-        override def apply(v1: String): Result = fn(v1)
-      }
-    }
-    def |(that: => Parser) = {
-      val fn : String => Result = input => this.apply(input) match{
+      })
+    def |(that: => Parser) : Parser =
+      new Parser(input => this.apply(input) match {
         case Failure(_) => that(input)
         case Success(rest) => Success(rest)
-      }
-      new Parser {
-        override def apply(v1: String): Result = fn(v1)
-      }
-    }
+      })
   }
-  lazy val letter = new Parser {
-    override def apply(v1: String): Result = {
-      if(v1.length == 0) Failure("no rest input")
-      else if(v1.charAt(0) >= 'A' && v1.charAt(0) <= 'Z' || v1.charAt(0) >= 'a' && v1.charAt(0) <= 'z') Success(v1.substring(1))
-      else Failure(v1.charAt(0) + " is not a letter")
-    }
-  }
-  lazy val digit = new Parser {
-    override def apply(v1: String): Result = {
-      if(v1.length == 0) Failure("no rest input")
-      else if(v1.charAt(0) >= '0' && v1.charAt(0) <= '9') Success(v1.substring(1))
-      else Failure(v1.charAt(0) + " is not a digit")
-    }
-  }
-  lazy val UNDERSCORE = new Parser {
-    override def apply(v1: String): Result = {
-      if(v1.length == 0) Failure("no rest input")
-      else if(v1.charAt(0) == '_') Success(v1.substring(1))
-      else Failure(v1.charAt(0) + " is not a '_'")
-    }
-  }
+  lazy val letter : Parser = new Parser(input => {
+      if(input.length == 0) Failure("no rest input")
+      else if(input.charAt(0) >= 'A' && input.charAt(0) <= 'Z' || input.charAt(0) >= 'a' && input.charAt(0) <= 'z') Success(input.substring(1))
+      else Failure(input.charAt(0) + " is not a letter")
+    })
+  lazy val digit : Parser = new Parser(input => {
+      if(input.length == 0) Failure("no rest input")
+      else if(input.charAt(0) >= '0' && input.charAt(0) <= '9') Success(input.substring(1))
+      else Failure(input.charAt(0) + " is not a digit")
+    })
+  lazy val UNDERSCORE : Parser = new Parser(input => {
+      if(input.length == 0) Failure("no rest input")
+      else if(input.charAt(0) == '_') Success(input.substring(1))
+      else Failure(input.charAt(0) + " is not a '_'")
+    })
   lazy val identifierLoop : Parser = UNDERSCORE.? ~ (letter | digit) ~ identifierLoop.?
-  lazy val identifier = letter ~ identifierLoop.?
+  lazy val identifier : Parser = letter ~ identifierLoop.?
 }
