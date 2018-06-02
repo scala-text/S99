34283e0cc935e16cd0d67a4584af5a1bd5f5e79a
Kota Mizushima <mizukota@gmail.com>
2018-06-02 21:01:03 +0900

    Add an answer of P68

---
+++ src/main/scala/jp/co/dwango/s99/P68.scala
@@ -0,0 +1,23 @@
+package jp.co.dwango.s99
+
+import jp.co.dwango.s99.binary_trees._
+
+object P68 {
+  implicit class RichTree[T](tree: Tree[T]) {
+    def preorder: List[T] = tree match {
+      case Node(v, l, r) =>
+        List(v) ++ l.preorder ++ r.preorder
+      case End =>
+        Nil
+    }
+    def inorder: List[T] = tree match {
+      case Node(v, l, r) =>
+        l.inorder ++ List(v) ++ r.inorder
+      case End =>
+        Nil
+    }
+  }
+  object Tree {
+    def string2Tree(str: String): Node[Char] = P67.Tree.fromString(str)
+  }
+}
